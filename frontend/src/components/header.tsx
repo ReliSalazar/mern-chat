1dd69cde21ce7a776afb6a86af143708f392f13f
Reli <naitvenefy@gmail.com>
2024-04-03 12:17:10 -0600

    theme changer

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,18 +4,30 @@ interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
   return (
-    <select className="gradientselect" data-choose-theme>
-      <option disabled value="">
-        Pick a theme
-      </option>
-      <option value="">Default</option>
-      <option value="light">Light</option>
-      <option value="cupcake">Cupcake</option>
-      <option value="emerald">Emerald</option>
-      <option value="dark">Dark</option>
-      <option value="sunset">Sunset</option>
-      <option value="dracula">Dracula</option>
-    </select>
+    <div className="navbar bg-base-300">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">MERN Chat</a>
+      </div>
+
+      <div className="navbar-end">
+        <select
+          className="select select-bordered w-full max-w-xs"
+          data-choose-theme
+          data-key="theme"
+        >
+          <option disabled selected value="">
+            Pick a theme
+          </option>
+          <option value="">Default</option>
+          <option value="light">Light</option>
+          <option value="cupcake">Cupcake</option>
+          <option value="emerald">Emerald</option>
+          <option value="dark">Dark</option>
+          <option value="sunset">Sunset</option>
+          <option value="dracula">Dracula</option>
+        </select>
+      </div>
+    </div>
   );
 };
 
