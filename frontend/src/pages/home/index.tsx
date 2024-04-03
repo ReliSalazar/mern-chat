a750d2316e56573f374ceb170fd6ab4958702cf3
Reli <naitvenefy@gmail.com>
2024-04-03 13:35:00 -0600

    init home and signup pages

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface HomeProps {}
+
+const Home: React.FC<HomeProps> = () => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/signup/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface SignUpProps {}
+
+const SignUp: React.FC<SignUpProps> = () => {
+  return (
+    <div>
+      <p>SignUp</p>
+    </div>
+  );
+};
+
+export default SignUp;
