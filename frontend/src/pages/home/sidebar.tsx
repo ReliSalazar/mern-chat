b1e88b814d71053f47ad2b787c4ec3d7ca0840c5
Reli <naitvenefy@gmail.com>
2024-04-05 10:46:23 -0600

    search

--- frontend/src/pages/home/sidebar.tsx
+++ frontend/src/pages/home/sidebar.tsx
@@ -3,24 +3,40 @@ import SearchInput from "../../components/search-input";
 import LogoutIcon from "../../icons/logout";
 import useLogOut from "../../hooks/use-log-out";
 import Conversations from "./conversations";
+import useConversation from "../../store/use-conversation";
+import useGetConversations from "../../hooks/use-get-conversation";
 
 interface SidebarProps {}
 
 const Sidebar: React.FC<SidebarProps> = () => {
-  const [value, setValue] = useState<string>("");
+  const [search, setSearch] = useState<string>("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
   const { loading, logOut } = useLogOut();
 
   const handleSearch = () => {
-    console.log(value);
+    if (search.trim() === "") return;
+    const conversation = conversations.find(
+      (conversation) =>
+        conversation.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    }
   };
 
   return (
     <div className="pt-2 px-4 flex flex-col divide-y divide-neutral gap-2">
-      <SearchInput value={value} onChange={setValue} onEnter={handleSearch} />
+      <SearchInput value={search} onChange={setSearch} onEnter={handleSearch} />
       <Conversations />
       <div className="flex items-center py-2">
         <button onClick={logOut} disabled={loading} className="btn w-full">
-          {loading ? <span className="loading loading-spinner w-4 h-4" /> : <LogoutIcon className="w-4 h-4" />}
+          {loading ? (
+            <span className="loading loading-spinner w-4 h-4" />
+          ) : (
+            <LogoutIcon className="w-4 h-4" />
+          )}
           <span className="mb-1">logout</span>
         </button>
       </div>
