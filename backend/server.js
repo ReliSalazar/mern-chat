c21003aa370a934f240022b1f230ddfe0f4605b2
Reli <naitvenefy@gmail.com>
2024-04-05 10:53:13 -0600

    initialize sockets on backend

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ import connectToMongoDB from "./db/connect-to-mongo-db.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -18,7 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
